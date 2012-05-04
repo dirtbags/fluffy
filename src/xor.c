b56f6dfb708a91516c5060c3b057ef2adf21b753
pi-rho <github@tyr.cx>
2012-05-03 20:25:20 -0500

    * Added PDF document creation (groff -> ps2pdf)   - renamed man pages
    * Added additional functionality to xor. This tool will be installed with
      several names to change its core action, while retaining its overall
      functionality. Currently, the following actions have been included:
      - xor
      - rot (bitwise rotate)
      - rol (bytewise rotate)
      - caesar (Caesar's cipher)
    
    * Added a summary man page (netre-tools.mdoc). This man page will be a sort of
      menu for users to find all of the tools included in the toolkit. Suggested
      by cashmoney.
    
    * Added additional code in Makefile.am to detect GCC and to handle debug

--- src/xor.c
+++ src/xor.c
@@ -1,11 +1,11 @@
 /*
- *                    __ _ _ _
- * __  _____  _ __   / _(_) | |_ ___ _ __
- * \ \/ / _ \| '__| | |_| | | __/ _ \ '__|
- *  >  < (_) | |    |  _| | | ||  __/ |
- * /_/\_\___/|_|    |_| |_|_|\__\___|_|
+ *   _                        __
+ *  | |_ _ __ __ _ _ __  ___ / _| ___  _ __ _ __ ___   ___ _ __
+ *  | __| '__/ _` | '_ \/ __| |_ / _ \| '__| '_ ` _ \ / _ \ '__|
+ *  | |_| | | (_| | | | \__ \  _| (_) | |  | | | | | |  __/ |
+ *   \__|_|  \__,_|_| |_|___/_|  \___/|_|  |_| |_| |_|\___|_|
  *
- * apply mask bytes to the pipeline
+ * apply bytes to the pipeline
  *
  */
 
@@ -19,60 +19,114 @@
 #include <getopt.h>
 #include "netre.h"
 
-int version(bool error) {
-    fprintf(WHICHOUT(error), "xor v.%s - %s\n\n", PACKAGE_VERSION,
-            "apply mask bytes to the pipeline, using XOR");
+typedef enum transform {
+    xor,
+    rot,
+    rol,
+    caesar
+} transform_t;
+
+static const char *ttoa(transform_t p) {
+    switch(p) {
+        case xor: return "xor"; break;
+        case rot: return "rot"; break;
+        case rol: return "rol"; break;
+        case caesar: return "caesar"; break;
+        default : return "---"; break;
+    }
+}
+
+transform_t parse_tname(char *arg) {
+    char *t = basename(arg);
+
+    if (!strncasecmp(t, "rot", 3)) return rot;
+    if (!strncasecmp(t, "rol", 3)) return rol;
+    if (!strncasecmp(t, "caesar", 6)) return caesar;
+    return xor;
+}
+
+int version(bool error, transform_t t) {
+    fprintf(WHICHOUT(error), "%s v.%s - %s %s\n\n", ttoa(t), PACKAGE_VERSION,
+            "apply bytes to the pipeline, using", ttoa(t));
     return error ? EX_USAGE : EX_OK;
 }
 
-int usage(bool error, char *prog) {
-    int retval = version(error);
-    fprintf(WHICHOUT(error), "Usage:\t%s [-x] MASK [MASK]*\n", prog);
-    fprintf(WHICHOUT(error), "\t%s  -s  MASKSTRING\n", prog);
-    fprintf(WHICHOUT(error), "\t-x\tmask bytes are hexadecimal\n");
-    fprintf(WHICHOUT(error), "\t-s\tuse a string of characters as a mask\n");
-    fprintf(WHICHOUT(error), "\n\tMASK\t\ta mask byte\n");
-    fprintf(WHICHOUT(error), "\tMASKSTRING\ta string of mask characters\n");
+int usage(bool error, transform_t t) {
+    int retval = version(error, t);
+    fprintf(WHICHOUT(error), "Usage:\t%s [-x] BYTE [BYTE]*\n", ttoa(t));
+    fprintf(WHICHOUT(error), "\t%s -s  BYTESTRING\n", ttoa(t));
+    fprintf(WHICHOUT(error), "\t-s\tuse a string of characters as the byte source\n");
+    fprintf(WHICHOUT(error), "\t-x\tbytes are hexadecimal\n");
+    if (t != xor)
+        fprintf(WHICHOUT(error), "\t-u\tundo %s\n", ttoa(t));
     return retval;
 }
 
-void do_xor(uint8_t mask[], int len) {
+#define XOR(c,s)          (c ^ s)
+#define ROTL(c,s)         ((c >> (s%8)) | (c << (8-(s%8)))) & 0xFF
+#define ROTR(c,s)         ((c << (s%8)) | (c >> (8-(s%8)))) & 0xFF
+#define ROT(d,c,s)        (d ? ROTR(c,s) : ROTL(c,s))
+#define ROLL(c,s)         (c - s) % 256
+#define ROLR(c,s)         (c + s) % 256
+#define ROL(d,c,s)        (d ? ROLR(c,s) : ROLL(c,s))
+#define CAESARE(c,s,a)    ((c - a + s) % 26) + a
+#define CAESARD(c,s,a)    ((c - a - s) % 26) + a
+#define CAESAR(d,c,s,a)   (d ? CAESARD(c,s,a) : CAESARE(c,s,a))
+
+int CAE(int undo, int c, uint8_t s) {
+    if (isupper(c)) return CAESAR(undo, c, s, 'A');
+    if (islower(c)) return CAESAR(undo, c, s, 'a');
+    return c;
+}
+
+int action(transform_t t, uint8_t mask[], int len, bool undo) {
     int i = 0;
 
     while (true) {
         int c = getchar();
+        if (c == EOF) break;
 
-        if (EOF == c) break;
-
-        c ^= mask[i];
+        switch(t) {
+            case rot:    c = ROT(undo, c, mask[i]); break;
+            case rol:    c = ROL(undo, c, mask[i]); break;
+            case caesar: c = CAE(undo, c, mask[i]); break;
+            default:     c = XOR(      c, mask[i]); break;
+        }
 
         putchar(c);
         i = (i + 1) % len;
     }
+
+    return EX_OK;
 }
 
 int main(int argc, char *argv[]) {
-    int opt;
-    int masklen = 0;
-    int base    = 0;
     int i;
+    int opt;
+    int masklen   = 0;
+    int base      = 0;
+    bool undo     = false;
+    transform_t t = parse_tname(argv[0]);
 
     /* option parsing */
-    while ((opt = getopt(argc, argv, "xsvh")) != -1) {
+    while ((opt = getopt(argc, argv, "uxsvh")) != -1) {
         switch (opt) {
-            case 'x':       /* hex  */
+            case 'u':                        /* undo */
+                if (t != xor) undo = true;
+                break;
+            case 'x':                        /* hex  */
                 base = 16;
                 break;
-            case 's':       /* string */
+            case 's':                        /* string */
                 base = -1;
                 break;
-            case 'v': return version(false);
-            case 'h': return usage(false, argv[0]);
-            default:  return usage(true,  argv[0]);
+            case 'v': return version(false, t);
+            case 'h': return   usage(false, t);
+            default:  return   usage(true,  t);
         }
     }
 
-    if (optind >= argc) return usage(true, argv[0]);
+    if (optind >= argc) return usage(true, t);
 
     if (base != -1) {
         masklen = argc - optind;
@@ -80,15 +134,13 @@ int main(int argc, char *argv[]) {
         for (i = optind; i < argc; i++)
             mask[i-optind] = (uint8_t)strtol(argv[i], NULL, base);
 
-        do_xor(mask, masklen);
-    } else { /* string */
+        return action(t, mask, masklen, undo);
+    } else {                                /* string */
         masklen = strlen(argv[optind]);
         uint8_t mask[masklen];
         for (i = 0; i < masklen; i++)
             mask[i] = (uint8_t)argv[optind][i];
 
-        do_xor(mask, masklen);
+        return action(t, mask, masklen, undo);
     }
-
-    return EX_OK;
 }
