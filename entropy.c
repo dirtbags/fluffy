cd3d143d841822acfe0c86e02731a40c437c9a9e
Neale Pickett <neale@lanl.gov>
2018-01-08 23:39:44 +0000

    Add entropy tool

---
+++ entropy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int
+main(int argc, char *argv[])
+{
+	size_t len = 0;
+	size_t hist[256] = {0};
+	int c;
+
+	while ((c = getchar()) != EOF) {
+		hist[c] += 1;
+		len += 1;
+	}
+
+	float H = 0;
+	for (int i = 0; i < 256; i += 1) {
+		if (hist[i]) {
+			float Hi = (float)hist[i]/len;
+			H -= Hi * log2(Hi);
+		}
+	}
+
+	printf("%f\n", H);
+}
+
