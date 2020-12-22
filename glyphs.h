d9490baf941a2f8cfdc85ef5ccfe36d6dd2ff926
Neale Pickett <neale@lanl.gov>
2020-12-22 13:06:21 -0700

    Update glyphs for 2020

--- glyphs.h
+++ glyphs.h
@@ -1,9 +1,27 @@
 #pragma once
 
-/* These glyphs are in most monospace fonts I tried in 2018 */
+/* List of visually distinct glyphs.
+ *
+ * The goal here is visually distinct, 
+ * not perfect fidelity with the Color Graphics Adapter from 1981,
+ * although it certainly borrows heavily from CP437.
+ * Also I refuse to give up the first 16, 
+ * since I've been seeing them for 40 years.
+ * 
+ * It's 2021 and the state of monospace glyphs is a mess.
+ * Not that I blame anybody. 
+ * The notion of monospacing needs to go away.
+ * I'm going to need to figure out a smarter way to display 
+ * grids of characters in a terminal.
+ * 
+ * Anyway, this is the best I could get. It looks okay in Cousine.
+ * It looks horrible in Source Code Pro, which I think is borrowing
+ * glyphs from Noto Mono.
+ * 
+ *  */
 const char *fluffyglyphs[] = {
 	"·", "☺", "☻", "♥", "♦", "♣", "♠", "•", "◘", "○", "◙", "♂", "♀", "♪", "♫", "☼",
-	"►", "◄", "↕", "‼", "¶", "§", "▬", "↨", "↑", "↓", "→", "←", "∟", "↔", "▲", "▼",
+	"⏵", "⏴", "↕", "‼", "¶", "§", "‽", "↨", "↑", "↓", "→", "←", "∟", "↔", "⏶", "⏷",
 	" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/",
 	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?",
 	"@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
@@ -18,5 +36,5 @@ const char *fluffyglyphs[] = {
 	"└", "┴", "┬", "├", "─", "┼", "╞", "╟", "╚", "╔", "╩", "╦", "╠", "═", "╬", "╧",
 	"╨", "╤", "╥", "╙", "╘", "╒", "╓", "╫", "╪", "┘", "┌", "█", "▄", "▌", "▐", "▀",
 	"α", "ß", "Γ", "π", "Σ", "σ", "µ", "τ", "Φ", "Θ", "Ω", "δ", "∞", "φ", "ε", "∩",
-	"≡", "±", "≥", "≤", "⌠", "⌡", "÷", "≈", "°", "∀", "∃", "√", "ⁿ", "²", "■", "¤",
+	"≡", "±", "≥", "≤", "⌠", "⌡", "÷", "≈", "°", "∞", "⊻", "√", "ⁿ", "²", "■", "¤",
 };
